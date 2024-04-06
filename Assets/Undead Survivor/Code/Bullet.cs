825e0cb1195eacd6b35ddf605f6767a734557689
Seok-SangHun <sanghun1016@naver.com>
2024-04-06 10:09:49 +0900

    day3

--- Assets/Undead Survivor/Code/Bullet.cs
+++ Assets/Undead Survivor/Code/Bullet.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -7,9 +8,35 @@ public class Bullet : MonoBehaviour
     public float damage;
     public int per;
 
-    public void Init(float damage, int per)
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+       rigid = GetComponent<Rigidbody2D>(); 
+    }
+
+    public void Init(float damage, int per, Vector3 dir) // 데미지, 관통값, 방향
     {
         this.damage = damage;
         this.per = per;
+
+        if(per > -1) //-1보다 크면 원거리, 큰것에 대해서 속도 적용
+        {
+            rigid.velocity = dir * 15f; //총알 날아가는 속도
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy") || per == -1)//|| = or, per가 -1이면 아래 로직 실행할 필요 x 
+            return;
+        
+        per--;
+
+        if(per == -1)
+        {
+            rigid.velocity = Vector2.zero;
+            gameObject.SetActive(false);// 관통값이 줄다가 -1 되면 비활성화
+        }
     }
 }
--- Assets/Undead Survivor/Code/Weapon.cs
+++ Assets/Undead Survivor/Code/Weapon.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
@@ -10,7 +11,15 @@ public class Weapon : MonoBehaviour
     public int count;
     public float speed;
 
-    private void Start()
+    float timer;
+    Player player;
+
+    void Awake()
+    {
+        player = GetComponentInParent<Player>();//부모 컨포넌트 가져오기
+    }
+
+    void Start()
     {
         Init();
     }
@@ -20,11 +29,31 @@ public class Weapon : MonoBehaviour
         {
             case 0:
                 transform.Rotate(Vector3.back * speed * Time.deltaTime);//deltaTime 한 프레임이 소비한 시간
-
                 break;
             default:
+                timer += Time.deltaTime;
+
+                if(timer > speed)
+                {
+                    timer = 0f;
+                    Fire();
+                }
                 break;
         }
+
+        //test code
+        if (Input.GetButtonDown("Jump"))
+        {
+            LevelUp(10, 1);
+        }
+    }
+    public void LevelUp(float damage, int count)
+    {
+        this.damage = damage;
+        this.count += count;
+
+        if (id == 0)
+            Batch();
     }
 
     public void Init()
@@ -34,9 +63,9 @@ public class Weapon : MonoBehaviour
             case 0:
                 speed = 150; // "-" 해야 시계 방향으로 돌음
                 Batch();
-                
                 break;
             default:
+                speed = 0.3f; // 1초에 3발씩
                 break;
         }
     }
@@ -45,9 +74,38 @@ public class Weapon : MonoBehaviour
     {
         for(int index = 0; index < count; index++)
         {
-            Transform bullet = GameManager.Instance.pool.Get(prefabId).transform;
-            bullet.parent = transform;
-            bullet.GetComponent<Bullet>().Init(damage, -1);//-1 is Infinity Per.
+            Transform bullet; //기존 오브젝트를 먼저 활용, 모자란 것은 풀링에서 가져옴
+            if(index < transform.childCount)
+            {
+                bullet = transform.GetChild(index);
+            }
+            else
+            {
+                bullet = GameManager.Instance.pool.Get(prefabId).transform;
+                bullet.parent = transform;
+            }
+
+            bullet.localPosition = Vector3.zero;
+            bullet.localRotation = Quaternion.identity; // 회전 000으로 초기화
+
+            Vector3 rotVec = Vector3.forward * 360 * index / count; // 처음은 0, 두번째 36, 세번째 72
+            bullet.Rotate(rotVec); // Rotate : 함수로 계산된 각도 적용
+            bullet.Translate(bullet.up * 1.5f, Space.World); // 자신의 위쪽으로 이동, 1.5플레이어와 거리, space world는 이동방향
+            bullet.GetComponent<Bullet>().Init(damage, -1, Vector3.zero);//-1 is Infinity Per.
         }
+    }
+    void Fire()
+    {
+        if (!player.scanner.nearestTarget) //대상이 없다면
+            return;
+
+        Vector3 targetPos = player.scanner.nearestTarget.position; //위치
+        Vector3 dir = targetPos - transform.position; //크기가 포함된 방향: 목표위치 - 나의 위치
+        dir = dir.normalized;//현재 벡터의 방향은 유지하고 크기를 1로 변환된 속성
+
+        Transform bullet = GameManager.Instance.pool.Get(prefabId).transform;
+        bullet.position = transform.position; //플레이어 위치
+        bullet.rotation = Quaternion.FromToRotation(Vector3.up, dir); //지정된 축을 중심으로 목표를 향해 회전하는 함수
+        bullet.GetComponent<Bullet>().Init(damage, count, dir);
     }
 }
